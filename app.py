235e7d2de723d38f0823fbaf3d4fee3cfa86d205
Teo Calvo <teocalvo2@gmail.com>
2026-09-04 09:39:46 -0300

    feat: auto caption

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
+# %%
+# 
 import streamlit as st
 import whisper
 import os
+import dotenv
+
+dotenv.load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+from google import genai
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+# %%
+
+@st.cache_resource(ttl='1h')
+def generate_caption(roteiro: str, model:str):
+
+    config = {
+        "temperature": 0.1,
+        "top_p": 0.95,
+        "system_instruction": "Você é um especialista em criação de legendas de posts em redes sociais. A partir do roteiro fornecido, crie uma legenda envolvente que chame atenção do público, bem como as devidas hashtags para engajamento. O tamanho máximo do post deve ser de 280 caracteres."
+    }
+
+    content = client.models.generate_content(
+        model=model,
+        contents=f"Aqui vai o roteiro do post...\n {roteiro}",
+        config=config,
+    )
+    
+    return content
 
 
 @st.cache_resource(ttl='1h')
@@ -54,9 +83,13 @@ if uploaded_file is not None:
         txt = result["text"]
         st.text_area("Transcrição", txt, height=300)
 
-
     new_file = uploaded_file.split(".")[:-1]
     new_file = ".".join(new_file) + ".txt"
     if st.button("Baixar Transcrição"):
         with open(new_file, "w", encoding="utf-8") as f:
             f.write(txt if time_mark else result["text"])
+
+    models = [m.name for m in client.models.list() if 'gemini-3' in m.name]
+    model = st.selectbox("Escolha o modelo para gerar a legenda", models, index=0)
+    response = generate_caption(txt, model=model)
+    st.text_area("Legenda Gerada", response.text, height=200)
